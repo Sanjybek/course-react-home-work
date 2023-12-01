78a2efd8d265657ba2f593d1a36fc21b57f45a69
Sanjybek <sanjybek003@gmail.com>
2023-12-01 16:13:26 +0600

    redux

--- src/App.jsx
+++ src/App.jsx
@@ -12,19 +12,7 @@ import { router } from './Components/page/routers';
 function App() {
   return <RouterProvider router={router} />
 
-import Resume from './Components/Resume/Resume';
-import TodoListRedux from './Components/TodoListRedux/TodoList'
-function App() {
 
-  return (
-   <>
-      <TodoListRedux/>
-      {/* <TodoList/>
-      <Calculator/> */}
-      {/* <Resume/> */}
-     
-   </>
-  );
 
 }
 
