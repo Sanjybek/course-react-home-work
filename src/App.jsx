cebe5696b80953e0636433a3b20beb929110f83e
Sanjybek <sanjybek003@gmail.com>
2023-12-01 19:17:11 +0600

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,7 @@
 import React from 'react';
 import './App.css';
-
-
-import {RouterProvider} from "react-router-dom";
-import { router } from './page/routers';
-
-
-
-
-
 function App() {
   return <RouterProvider router={router} />
-
-// import Resume from './Components/Resume/Resume';
-// import TodoListRedux from './Components/TodoListRedux/TodoList'
-// function App() {
-
-//   return (
-//    <>
-//       <TodoListRedux/>
-//       {/* <TodoList/>
-//       <Calculator/> */}
-//       {/* <Resume/> */}
-     
-//    </>
-//   );
-
 }
 
 export default App;
--- src/page/TodoListPage/TodoListPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import TodoListRedux from '../../Components/TodoListRedux/TodoListRedux';
-
-const TodoListPage = () => {
-    return <TodoListRedux/>
-    
-};
-
-export default TodoListPage;
